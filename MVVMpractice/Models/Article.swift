0d95892c9f4bb7e78fee33d488e65602bf69ab45
minhye9731 <jenney62639731@gmail.com>
2023-01-21 12:10:02 +0900

    Feat: 데이터 구조체 생성

---
+++ MVVMpractice/Models/Article.swift
@@ -0,0 +1,13 @@
+//
+//  Article.swift
+//  MVVMpractice
+//
+//  Created by 강민혜 on 1/21/23.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    let title: String
+    let description: String
+}
