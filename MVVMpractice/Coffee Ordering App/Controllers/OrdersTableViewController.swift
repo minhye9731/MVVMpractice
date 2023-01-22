e679e4c7658172fb7cfcf3975b7a01ce31c8d6fa
minhye9731 <jenney62639731@gmail.com>
2023-01-23 00:20:09 +0900

    Feat: Order 네트워크 모델 추가 및 서버 연결

--- MVVMpractice/Coffee Ordering App/Controllers/OrdersTableViewController.swift
+++ MVVMpractice/Coffee Ordering App/Controllers/OrdersTableViewController.swift
@@ -10,7 +10,28 @@ import UIKit
 
 class OrdersTableViewController: UITableViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populateOrders()
+    }
     
+    private func populateOrders() {
+        guard let coffeeOrdersURL = URL(string: "https://warp-wiry-rugby.glitch.me/orders") else {
+            fatalError("URL was incorrect")
+        }
+        
+        let resource = Resource<[Order]>(url: coffeeOrdersURL)
+        
+        OrderWebservice().load(resource: resource) { result in
+            
+            switch result {
+            case .success(let orders):
+                print(orders)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
     
     
 }
