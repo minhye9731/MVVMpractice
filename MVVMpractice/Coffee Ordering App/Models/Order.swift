ea1f5fb6f202e7f9cdeb669bcd495f369789fda1
minhye9731 <jenney62639731@gmail.com>
2023-01-23 00:17:57 +0900

    Feat: Coffee 주문 데이터 모델 추가

---
+++ MVVMpractice/Coffee Ordering App/Models/Order.swift
@@ -0,0 +1,30 @@
+//
+//  Order.swift
+//  MVVMpractice
+//
+//  Created by 강민혜 on 1/22/23.
+//
+
+import Foundation
+
+enum CoffeeType: String, Codable {
+    case cappuccino
+    case latte
+    case espressino
+    case cortado
+}
+
+enum CoffeeSize: String, Codable {
+    case small
+    case medium
+    case large
+}
+
+struct Order: Codable {
+    
+    let name: String
+    let email: String
+    let type: CoffeeType
+    let size: CoffeeSize
+
+}
