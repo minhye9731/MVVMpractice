3827b02ce29e76b716fbb24b62fdc5fcb5829ff6
minhye9731 <jenney62639731@gmail.com>
2023-01-22 14:28:15 +0900

    Feat: 뉴스 데이터 전달 구현

---
+++ MVVMpractice/ViewModels/ArticleViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  ArticleViewModel.swift
+//  MVVMpractice
+//
+//  Created by 강민혜 on 1/21/23.
+//
+
+import Foundation
+
+// MARK: - ArticleListViewModel
+struct ArticleListViewModel {
+    let articles: [Article]
+}
+
+extension ArticleListViewModel {
+    
+    var numberOfSections: Int {
+        return 1
+    }
+    
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        return self.articles.count
+    }
+    
+    func articleAtIndex(_ index: Int) -> ArticleViewModel {
+        let article = self.articles[index]
+        return ArticleViewModel(article)
+    }
+}
+
+
+// MARK: - ArticleViewModel
+struct ArticleViewModel {
+    private let article: Article
+}
+
+extension ArticleViewModel {
+    init(_ article: Article) {
+        self.article = article
+    }
+}
+
+extension ArticleViewModel {
+    
+    var title: String {
+        return self.article.title
+    }
+    
+    var description: String {
+        return self.article.description
+    }
+}
