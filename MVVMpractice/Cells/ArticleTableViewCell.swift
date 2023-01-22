ceb0e3af7478969eb3096642c4ae9af6314c09fc
minhye9731 <jenney62639731@gmail.com>
2023-01-22 14:19:44 +0900

    Feat: 기사 메인 cell 추가

---
+++ MVVMpractice/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  MVVMpractice
+//
+//  Created by 강민혜 on 1/22/23.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @ IBOutlet weak var titleLabel: UILabel!
+    @ IBOutlet weak var descriptionLabel: UILabel!
+    
+}
