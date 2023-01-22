4800792c2dc5cea7dfe50a2d43e44d00649e4b1e
minhye9731 <jenney62639731@gmail.com>
2023-01-22 14:32:46 +0900

    Feat: Main 네트워크 연결 및 데이터 전달 구현

--- MVVMpractice/Controllers/NewsListTableViewController.swift
+++ MVVMpractice/Controllers/NewsListTableViewController.swift
@@ -10,19 +10,55 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController {
     
+    private var articleListVM: ArticleListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
         setNavi()
     }
+}
+
+// MARK: - tableview 설정
+extension NewsListTableViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM.numberOfRowsInSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell not found")
+        }
+        let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.description
+        return cell
+    }
+}
+
+// MARK: - 기타 설정
+extension NewsListTableViewController {
     
     private func setup() {
         
         self.navigationController?.navigationBar.prefersLargeTitles = true
         
         let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=37aa51bb50c6467d81fc0956610ef893")!
-        Webservice().getArticles(url: url) { _ in
+        
+        Webservice().getArticles(url: url) { articles in
             
+            if let articles = articles {
+                self.articleListVM = ArticleListViewModel(articles: articles)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
         }
     }
     
@@ -34,11 +70,6 @@ class NewsListTableViewController: UITableViewController {
         navibarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
         
         self.navigationItem.scrollEdgeAppearance = navibarAppearance
-        self.navigationItem.standardAppearance = navibarAppearance // 여기서의 글자색상은 large말고 그냥 textattribute로 해야 함.
-        
+        self.navigationItem.standardAppearance = navibarAppearance
     }
-    
-    
-    
-    
 }
