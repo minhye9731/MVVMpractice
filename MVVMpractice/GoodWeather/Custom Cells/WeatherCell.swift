06304df1e0bf334104bcf2795c94090d8b869c3d
minhye9731 <jenney62639731@gmail.com>
2023-01-23 23:41:58 +0900

    Feat: 날씨 (도시명/기온) cell UI 구현

---
+++ MVVMpractice/GoodWeather/Custom Cells/WeatherCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherCell.swift
+//  MVVMpractice
+//
+//  Created by 강민혜 on 1/23/23.
+//
+
+import Foundation
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+}
