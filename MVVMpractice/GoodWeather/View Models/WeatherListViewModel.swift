6a45e33bca921ee7b6a1fb558f32c6d90a576e13
minhye9731 <jenney62639731@gmail.com>
2023-01-24 23:55:46 +0900

    Feat: 메인화면 날씨 tableview 구성 로직 구현

---
+++ MVVMpractice/GoodWeather/View Models/WeatherListViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  WeatherListViewModel.swift
+//  MVVMpractice
+//
+//  Created by 강민혜 on 1/24/23.
+//
+
+import Foundation
+
+class WeatherListViewModel {
+    
+    private var weatherViewModels = [WeatherViewModel]()
+    
+    func addWeatherViewModel(_ vm: WeatherViewModel) {
+        weatherViewModels.append(vm)
+    }
+    
+    func numberOfRows(_ sections: Int) -> Int {
+        return weatherViewModels.count
+    }
+    
+    func modelAt(_ index: Int) -> WeatherViewModel {
+        return weatherViewModels[index]
+    }
+    
+}
+
+class WeatherViewModel {
+    
+    let weather: WeatherResponse
+    
+    init(weather: WeatherResponse) {
+        self.weather = weather
+    }
+    
+    var city: String {
+        return weather.name
+    }
+    
+    var temperature: Double {
+        return weather.main.temp
+    }
+    
+}
