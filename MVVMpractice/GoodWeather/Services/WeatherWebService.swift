4e2c434f6bca7f33e20fdd88f2cbea66d2fefb44
minhye9731 <jenney62639731@gmail.com>
2023-01-24 23:59:10 +0900

    Refactor: 의미없는 print문 제거

--- MVVMpractice/GoodWeather/Services/WeatherWebService.swift
+++ MVVMpractice/GoodWeather/Services/WeatherWebService.swift
@@ -18,8 +18,6 @@ final class WeatherWebService {
         
         URLSession.shared.dataTask(with: resource.url) { data, response, error in
             
-            print(data)
-            
             if let data = data {
                 DispatchQueue.main.async {
                     completion(resource.parse(data))
