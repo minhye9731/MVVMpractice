a17e5c3f1f83f25209ec7f78a28d3999dc26bb7e
minhye9731 <jenney62639731@gmail.com>
2023-01-24 23:47:11 +0900

    Feat:  검색도시별 URL 생성 메서드 추가

---
+++ MVVMpractice/GoodWeather/Utils/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  MVVMpractice
+//
+//  Created by 강민혜 on 1/23/23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Urls {
+        
+        static func urlForWeatherByCity(city: String) -> URL {
+            
+//            get the default settings for temperature
+//            let userDefaults = UserDefaults.standard
+//            let unit = (userDefaults.value(forKey: "unit") as? String) ?? "imperial"
+                                        
+            return URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city.escaped())&appid=412f4b3ad7251a4cc5b26b8a732f1a4d&units=imperial")!
+        }
+    }
+}
