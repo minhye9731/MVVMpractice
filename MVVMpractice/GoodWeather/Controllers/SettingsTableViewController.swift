dbd8242b56e0c4a09aa27f513b946075824ba1a5
minhye9731 <jenney62639731@gmail.com>
2023-01-26 00:44:06 +0900

    Feat: 설정 Cell UI 구현

--- MVVMpractice/GoodWeather/Controllers/SettingsTableViewController.swift
+++ MVVMpractice/GoodWeather/Controllers/SettingsTableViewController.swift
@@ -10,4 +10,45 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
     
+    private var settingsViewModel = SettingsViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavi()
+    }
+    
+    private func setNavi() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.tintColor = .white
+        
+        let navibarAppearance = UINavigationBarAppearance()
+        navibarAppearance.backgroundColor = UIColor(displayP3Red: 52/225, green: 152/255, blue: 219/255, alpha: 1/0)
+        navibarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        navibarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+
+        self.navigationItem.scrollEdgeAppearance = navibarAppearance
+        self.navigationItem.standardAppearance = navibarAppearance
+    }
+    
+}
+
+extension SettingsTableViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(settingsViewModel.units.count)
+        return settingsViewModel.units.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let settingsItem = settingsViewModel.units[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell", for: indexPath)
+        cell.textLabel?.text = settingsItem.displayName
+        print(settingsItem.displayName)
+        return cell
+    }
 }
