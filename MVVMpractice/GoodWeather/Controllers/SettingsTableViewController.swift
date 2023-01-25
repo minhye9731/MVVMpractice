0122b1318def9ca6246daabd9536a447ace69821
minhye9731 <jenney62639731@gmail.com>
2023-01-25 23:47:54 +0900

    Feat: 설정화면 추가

---
+++ MVVMpractice/GoodWeather/Controllers/SettingsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.swift
+//  MVVMpractice
+//
+//  Created by 강민혜 on 1/25/23.
+//
+
+import Foundation
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+    
+}
