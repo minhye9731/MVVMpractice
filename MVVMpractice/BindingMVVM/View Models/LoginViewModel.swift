bef076151fcbe51541d6230606ca69d834e82217
minhye9731 <jenney62639731@gmail.com>
2023-01-28 23:59:03 +0900

    Feat: 로그인 ViewModel 생성

---
+++ MVVMpractice/BindingMVVM/View Models/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  MVVMpractice
+//
+//  Created by 강민혜 on 1/28/23.
+//
+
+import Foundation
+
+struct LoginViewModel {
+    var username: String = ""
+    var password: String = ""
+}
