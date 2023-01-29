7e883a6526fba297b17cf0ad35e6214a9f948777
minhye9731 <jenney62639731@gmail.com>
2023-01-29 22:32:30 +0900

    Feat: Generic 옵저버 생성

--- MVVMpractice/BindingMVVM/View Models/LoginViewModel.swift
+++ MVVMpractice/BindingMVVM/View Models/LoginViewModel.swift
@@ -7,7 +7,27 @@
 
 import Foundation
 
+class Dynamic<T> {
+    
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    func bind(callback: @escaping (T) -> Void) {
+        self.listener = callback
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+}
+
 struct LoginViewModel {
-    var username: String = ""
-    var password: String = ""
+    var username = Dynamic("")
+    var password = Dynamic("")
 }
