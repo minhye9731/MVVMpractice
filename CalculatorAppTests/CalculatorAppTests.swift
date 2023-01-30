35ee7476c3bdd55672da464b5968d3d3154a4857
minhye9731 <jenney62639731@gmail.com>
2023-01-30 23:43:49 +0900

    Test: Unit test 메서드 연습

--- CalculatorAppTests/CalculatorAppTests.swift
+++ CalculatorAppTests/CalculatorAppTests.swift
@@ -6,9 +6,22 @@
 //
 
 import XCTest
+@testable import MVVMpractice
 
 final class CalculatorAppTests: XCTestCase {
+    
+    
+    func test_SubtractTwoNumbers() {
+        let calculator = Calculator()
+        let result = calculator.subtract(5, 2)
+        XCTAssertEqual(result, 3)
+    }
 
- 
+    func test_AddTwoNumbers() {
+        let calculator = Calculator()
+        let result = calculator.add(2, 3)
+        
+        XCTAssertEqual(result, 5)
+    }
 
 }
---
+++ MVVMpractice/CalculatorApp/Calculator.swift
@@ -0,0 +1,19 @@
+//
+//  Calculator.swift
+//  MVVMpractice
+//
+//  Created by 강민혜 on 1/30/23.
+//
+
+import Foundation
+
+class Calculator {
+    
+    func add(_ a: Int, _ b: Int) -> Int {
+        return a+b
+    }
+    
+    func subtract(_ a: Int, _ b: Int) -> Int {
+        return a-b
+    }
+}
